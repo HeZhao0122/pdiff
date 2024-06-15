152b573ff62f8951528642aa5ae845d4f0c7d6fe
HeZhao0122 <75469405+HeZhao0122@users.noreply.github.com>
2024-06-15 19:33:15 +0800

    Update ae_ddpm.py

--- core/system/ae_ddpm.py
+++ core/system/ae_ddpm.py
@@ -28,8 +28,8 @@ class AE_DDPM(DDPM):
         # config.system.model.arch.model.in_dim = latent_dim[-1] * latent_dim[-2]
         super(AE_DDPM, self).__init__(config)
         # self.pos = self.task.get_pos().unsqueeze(0).float().cuda()
-        self.ae_model = torch.load('./param_data/AE_all.pt', map_location='cpu')
-        # self.ae_model = ae_model
+        # self.ae_model = torch.load('./param_data/AE_all.pt', map_location='cpu')
+        self.ae_model = ae_model
         self.save_hyperparameters()
         self.split_epoch = self.train_cfg.split_epoch
         self.dataset_list = self.train_cfg.datasets
